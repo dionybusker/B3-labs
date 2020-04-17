d8fa8e5f6a2f254f38abdee946c77ce6eab48b4a
dionybusker <99057959@mydavinci.nl>
2020-04-17 12:35:07 +0200

    in de header de title aangepast naar de juiste lab opdracht

--- B3W27/lab3-overzicht_locaties/includes/header.php
+++ B3W27/lab3-overzicht_locaties/includes/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>B3W27 - lab 1</title>
+        <title>B3W27 - lab 3</title>
         <link rel="stylesheet" href="css/style.css">
         <script src="https://kit.fontawesome.com/8eccf2802e.js" crossorigin="anonymous"></script> <!-- font awesome -->
     </head>
